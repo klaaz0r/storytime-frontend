2b736b30870adf40f5a6cd532b82c94961c6445f
klaas <klaas.foppen@gmail.com>
2016-02-02 12:52:41 +0100

    logging added

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
+
 var port = 3000;
 //serving the public folder
 app.use(express.static(__dirname + '/public'));
 
+//logging
+app.use(morgan(':remote-addr :method :url'));
+
+//start server
 app.listen(port, function() {
   console.log('app is running on port: ' + port);
 });

8d0889571c9f6f6489780f5f58b8cada4360d085
klaas <klaas.foppen@gmail.com>
2016-04-15 14:10:03 +0200

    fix footer to bottom

---
+++ src/app/components/navigation/footer.directive.js
@@ -0,0 +1,6 @@
+angular.module('app').directive('footer', function() {
+  return {
+    restrict: 'E',
+    templateUrl: '../app/components/navigation/footer.view.html'
+  };
+});
--- src/app/components/navigation/navbar.directive.js
+++ src/app/components/navigation/navbar.directive.js
@@ -1,7 +1,7 @@
 angular.module('app').directive('navbar', function() {
   return {
     restrict: 'E',
-    templateUrl: '../app/components/navbar/navbar.view.html',
+    templateUrl: '../app/components/navigation/navbar.view.html',
     controller: 'NavbarController'
   };
 });

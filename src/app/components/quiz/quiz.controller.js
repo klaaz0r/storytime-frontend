a70abe0569557fcf33c7f5fa7eb66ce9af6da785
Schultzie <adonthios@gmail.com>
2016-04-22 11:46:23 +0200

    Commit quiz init v2.0

--- src/app/components/quiz/quiz.controller.js
+++ src/app/components/quiz/quiz.controller.js
@@ -30,7 +30,7 @@ angular.module('app').controller('QuizController', ['$scope', function($scope) {
     name: 'De derde Quiz',
   }];
 
-  $scope.showQuestion = true;
+ /* $scope.showQuestion = true;
   $scope.toggleQuiz = function() {
     $scope.showQuestion = $scope.showQuestion === false ? true : false;
   };
@@ -38,5 +38,27 @@ angular.module('app').controller('QuizController', ['$scope', function($scope) {
   $scope.showAnswer = true;
   $scope.toggleQuestion = function() {
     $scope.showAnswer = $scope.showAnswer === false ? true : false;
-  };
+  };*/
+  
+  app.controller('QuizController', function ($scope) {
+	   
+  });
+  
+  app.directive('toggleInfo', function () {
+      var directive = {
+          restrict: 'A',
+          link: link
+      }
+
+      return directive;
+
+      function link(scope, element, attr) {
+          element.on('click', function () {
+              element.toggleClass('show');
+          });
+      }
+  })
+  
+  
+  
 }]);

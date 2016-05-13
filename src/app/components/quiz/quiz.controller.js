28802372f690bf0cd3a8207d839d19f0b4fd3a9f
klaas <klaas.foppen@gmail.com>
2016-05-13 13:31:14 +0200

    small style fix

--- src/app/components/quiz/quiz.controller.js
+++ src/app/components/quiz/quiz.controller.js
@@ -1,51 +1,45 @@
 angular.module('app').controller('QuizController', ['$scope', function($scope) {
-	$scope.credentials = {
-		    id: ''
-		  };
-	    $scope.quizes = [
-	        {
-	            name: 'De eerste Quiz',
-	            question : [
-	              {
-	            	  name: 'test question??',
-            		  answer : [
-            		              {
-            		            	  name: 'test answer??'
-            		              },
-            		              {
-            		            	  name: 'test answer??'
-            		              },
-            		              {
-            		            	  name: 'test answer??'
-            		              }
-            		            ]	  
-	              }
-	            ]
-	        },
-	        {
-	            name: 'De tweede Quiz',
-	            
-	            question : [
-	      	              {
-	      	            	  name: 'test question??',
-	                  		  answer : [
-	                  		              {
-	                  		            	  name: 'test answer??'
-	                  		              },
-	                  		              {
-	                  		            	  name: 'test answer??'
-	                  		              },
-	                  		              {
-	                  		            	  name: 'test answer??'
-	                  		              }
-	                  		            ]	  
-	      	              }
-	      	          ]
-	           },
-	        {
-	            name: 'De derde Quiz',
-	        }
-	    ];
-	    
-	    $scope.exampleModel = []; $scope.example1data = [ {id: 1, label: "David"}, {id: 2, label: "Jhon"}, {id: 3, label: "Danny"}];
+    $scope.credentials = {
+        id: ''
+    };
+    $scope.quizes = [{
+        name: 'De eerste Quiz',
+        question: [{
+            name: 'test question??',
+            answer: [{
+                name: 'test answer??'
+            }, {
+                name: 'test answer??'
+            }, {
+                name: 'test answer??'
+            }]
+        }]
+    }, {
+        name: 'De tweede Quiz',
+
+        question: [{
+            name: 'test question??',
+            answer: [{
+                name: 'test answer??'
+            }, {
+                name: 'test answer??'
+            }, {
+                name: 'test answer??'
+            }]
+        }]
+    }, {
+        name: 'De derde Quiz',
+    }];
+
+    $scope.exampleModel = [];
+    $scope.example1data = [{
+        id: 1,
+        label: "David"
+    }, {
+        id: 2,
+        label: "Jhon"
+    }, {
+        id: 3,
+        label: "Danny"
+    }];
 }]);

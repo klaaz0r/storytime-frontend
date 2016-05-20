ac9c88c5dc8cfb176a6438a986a91419a19edd03
klaas <klaas.foppen@gmail.com>
2016-05-20 09:27:11 +0200

    chat start quiz functions

--- src/app/app.js
+++ src/app/app.js
@@ -1,131 +1,131 @@
 var app = angular.module("app", ['ui.router', 'app.config', 'ngCookies', 'ngAnimate']);
 
 app.config(function($stateProvider, $urlRouterProvider, USER_ROLES) {
-  $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: '../app/components/static/home.view.html',
-      data: {
-        authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
-      }
-    })
-    .state('login', {
-      url: '/login',
-      templateUrl: '../app/components/login/login.view.html',
-      controller: 'LoginController',
-      data: {
-        authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
-      }
-    })
-    .state('chat', {
-      url: '/chat',
-      templateUrl: '../app/components/chat/chat.view.html',
-      controller: 'ChatController',
-      data: {
-    	  authorizedRoles: [USER_ROLES.all]
-      }
-    })
-    .state('dashboard', {
-      url: '/dashboard',
-      templateUrl: '../app/components/dashboard/dashboard.view.html',
-      controller: 'DashboardController',
-      data: {
-        authorizedRoles: [USER_ROLES.mentor]
-      }
-    })
-    .state('newchild', {
-      url: '/nieuwkind',
-      templateUrl: '../app/components/dashboard/newchild.view.html',
-      controller: 'DashboardController',
-      data: {
-        authorizedRoles: [USER_ROLES.mentor]
-      }
-    })
-    .state('register', {
-      url: '/register',
-      templateUrl: '../app/components/register/register.view.html',
-      controller: 'RegisterController',
-      data: {
-        authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
-      }
-    })
-    .state('forgetpassword', {
-      url: '/forgetpassword',
-      templateUrl: '../app/components/forgetpassword/forgetpassword.view.html',
-      controller: 'ForgetPasswordController',
-      data: {
-        authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
-      }
-    })
-    .state('newpassword', {
-      url: '/newpassword/:tokenId/:email',
-      templateUrl: '../app/components/forgetpassword/newpassword.view.html',
-      controller: 'NewPasswordController',
-      data: {
-        authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
-      }
-    })
-    .state('about', {
-      url: '/about',
-      templateUrl: '../app/components/static/about.view.html',
-      data: {
-        authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
-      }
-    })
-    .state('helpmee', {
-      url: '/helpus',
-      templateUrl: '../app/components/static/helpus.view.html',
-      data: {
-        authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
-      }
-    })
-    .state('styleguide', {
-      url: '/styleguide',
-      templateUrl: '../styleguide.html',
-      data: {
-        authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
-      }
-    })
-    .state('quiz', {
-      url: '/quiz',
-      templateUrl: '../app/components/quiz/quiz.view.html',
-      data: {
-        authorizedRoles: [USER_ROLES.all]
-      }
-    }).state('makeQuiz', {
-      url: '/quiz/maken',
-      templateUrl: '../app/components/quiz/makeQuiz.view.html',
-      data: {
-        authorizedRoles: [USER_ROLES.all]
-      }
-    })
-  $urlRouterProvider.otherwise("/");
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: '../app/components/static/home.view.html',
+            data: {
+                authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
+            }
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: '../app/components/login/login.view.html',
+            controller: 'LoginController',
+            data: {
+                authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
+            }
+        })
+        .state('chat', {
+            url: '/chat',
+            templateUrl: '../app/components/chat/chat.view.html',
+            controller: 'ChatController',
+            data: {
+                authorizedRoles: [USER_ROLES.all]
+            }
+        })
+        .state('dashboard', {
+            url: '/dashboard',
+            templateUrl: '../app/components/dashboard/dashboard.view.html',
+            controller: 'DashboardController',
+            data: {
+                authorizedRoles: [USER_ROLES.mentor]
+            }
+        })
+        .state('newchild', {
+            url: '/nieuwkind',
+            templateUrl: '../app/components/dashboard/newchild.view.html',
+            controller: 'DashboardController',
+            data: {
+                authorizedRoles: [USER_ROLES.mentor]
+            }
+        })
+        .state('register', {
+            url: '/register',
+            templateUrl: '../app/components/register/register.view.html',
+            controller: 'RegisterController',
+            data: {
+                authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
+            }
+        })
+        .state('forgetpassword', {
+            url: '/forgetpassword',
+            templateUrl: '../app/components/forgetpassword/forgetpassword.view.html',
+            controller: 'ForgetPasswordController',
+            data: {
+                authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
+            }
+        })
+        .state('newpassword', {
+            url: '/newpassword/:tokenId/:email',
+            templateUrl: '../app/components/forgetpassword/newpassword.view.html',
+            controller: 'NewPasswordController',
+            data: {
+                authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
+            }
+        })
+        .state('about', {
+            url: '/about',
+            templateUrl: '../app/components/static/about.view.html',
+            data: {
+                authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
+            }
+        })
+        .state('helpmee', {
+            url: '/helpus',
+            templateUrl: '../app/components/static/helpus.view.html',
+            data: {
+                authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
+            }
+        })
+        .state('styleguide', {
+            url: '/styleguide',
+            templateUrl: '../styleguide.html',
+            data: {
+                authorizedRoles: [USER_ROLES.all, USER_ROLES.mentor, USER_ROLES.child]
+            }
+        })
+        .state('quiz', {
+            url: '/quiz',
+            templateUrl: '../app/components/quiz/quiz.view.html',
+            data: {
+                authorizedRoles: [USER_ROLES.all]
+            }
+        }).state('makeQuiz', {
+            url: '/quiz/maken',
+            templateUrl: '../app/components/quiz/makeQuiz.view.html',
+            data: {
+                authorizedRoles: [USER_ROLES.all]
+            }
+        })
+    $urlRouterProvider.otherwise("/");
 
 });
 
 app.constant('USER_ROLES', {
-  all: '*',
-  mentor: 'MENTOR',
-  child: 'CHILD'
+    all: '*',
+    mentor: 'MENTOR',
+    child: 'CHILD'
 });
 
 app.run(function($rootScope, AuthService, $state, ErrorFactory, Session) {
-  $rootScope.$on('$stateChangeStart', function(event, next) {
-    var authorizedRoles = next.data.authorizedRoles;
+    $rootScope.$on('$stateChangeStart', function(event, next) {
+        var authorizedRoles = next.data.authorizedRoles;
 
-    if (!AuthService.isAuthorized(authorizedRoles)) {
+        if (!AuthService.isAuthorized(authorizedRoles)) {
 
-      event.preventDefault();
+            event.preventDefault();
 
-      if (AuthService.isAuthenticated()) {
-        // user is not allowed
-        ErrorFactory.setError('U bent niet ingelogd');
-        $state.go("login");
-      } else {
-        // user is not logged in
-        ErrorFactory.setError('U bent niet ingelogd');
-        $stateProvider.go("login");
-      }
-    }
-  });
+            if (AuthService.isAuthenticated()) {
+                // user is not allowed
+                ErrorFactory.setError('U bent niet ingelogd');
+                $state.go("login");
+            } else {
+                // user is not logged in
+                ErrorFactory.setError('U bent niet ingelogd');
+                $stateProvider.go("login");
+            }
+        }
+    });
 });
--- src/app/components/chat/chat.controller.js
+++ src/app/components/chat/chat.controller.js
@@ -1,5 +1,5 @@
 angular.module('app').controller('ChatController', ['$scope', function($scope, ngEnter) {
-	
+
     //init van een message, wat het kind stuurt is altijd basic plain tekst
     $scope.message = {
         text: ''
@@ -12,6 +12,17 @@ angular.module('app').controller('ChatController', ['$scope', function($scope, n
     }, {
         "author": "child",
         "text": "met mij best goed"
+    }, {
+        "author": "robin",
+        "text": "waar mee kan ik je helpen?",
+        "quiz": {
+            "title": "Verjaardag feestje",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quam est, ultrices ac tellus consequat",
+            "image": "https://s-media-cache-ak0.pinimg.com/736x/70/cc/40/70cc40437e3b042d26fa826a16482600.jpg"
+        }
+    }, {
+        "author": "robin",
+        "text": "waar mee kan ik je helpen?"
     }, {
         "author": "robin",
         "text": "waar mee kan ik je helpen?"
@@ -30,38 +41,41 @@ angular.module('app').controller('ChatController', ['$scope', function($scope, n
         //setten van de values
         message.text = text;
         message.author = 'child';
-        
+
         //hier tussen moet nog een service komen voor het versturen naar de server
         //push de message in de array zodat hij ook op het scherm getoond word
         $scope.messages.push(message);
-        
+
         // Deze moet pas aangeroepen worden nadat de DOM is geupdate.
         // Hoe kan dat?
         updateScroll();
+        console.log("update scroll");
     };
-    
+
     var scrolled = false;
-	function updateScroll(){
-	   if(!scrolled){
-	 	  console.log("Scrolled automaticly down");
-	 	  //$(".chat").scrollTop();
-	 	  $(".chat").animate({
-	 		  scrollTop: $(".chat")[0].scrollHeight },
-	 		  300
-	 	  );
-	 	  return false;
-	   }
-	}
-	 
-	$('.chat').bind('scroll', function() {
-	  if($(this).scrollTop() + $(this).innerHeight()>=$(this)[0].scrollHeight) {
-	 	 console.log('Bottom reached, Auto scroll enabled');
-	 	 scrolled=false;
-	  } else {
-		  console.log('Not at bottom, Auto scroll disabled');
-		  scrolled=true;
-	    }
-	 })
-	 
-	setInterval(function(){ updateScroll() }, 1000);
+
+    function updateScroll() {
+        if (!scrolled) {
+            console.log("Scrolled automaticly down");
+            //$(".chat").scrollTop();
+            $(".chat").animate({
+                    scrollTop: $(".chat")[0].scrollHeight
+                },
+                300
+            );
+            return false;
+        }
+    }
+
+    $('.chat').bind('scroll', function() {
+        if ($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
+            console.log('Bottom reached, Auto scroll enabled');
+            scrolled = false;
+        } else {
+            console.log('Not at bottom, Auto scroll disabled');
+            scrolled = true;
+        }
+    })
+
+    // setInterval(function(){ updateScroll() }, 1000);
 }]);

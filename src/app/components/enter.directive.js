ea01851f2f7d9fbb515cb4fc83a6f220f7c95960
klaas <klaas.foppen@gmail.com>
2016-05-23 17:14:58 +0200

    ng enter directive

--- src/app/components/enter.directive.js
+++ src/app/components/enter.directive.js
@@ -1,9 +1,11 @@
-app.directive('ngEnter', function() {
+angular.module('app').directive('ngEnter', function() {
     return function(scope, element, attrs) {
         element.bind("keydown keypress", function(event) {
             if (event.which === 13) {
                 scope.$apply(function() {
-                    scope.$eval(attrs.ngEnter);
+                    scope.$eval(attrs.ngEnter, {
+                        'event': event
+                    });
                 });
 
                 event.preventDefault();

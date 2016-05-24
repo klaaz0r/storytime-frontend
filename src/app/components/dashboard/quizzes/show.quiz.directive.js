a57f218ecb809f4b3a0709c059873f118f31f479
klaas <klaas.foppen@gmail.com>
2016-05-24 19:50:24 +0200

    created new folders in dashboard, it's getting out of hand

--- src/app/components/dashboard/quizzes/show.quiz.directive.js
+++ src/app/components/dashboard/quizzes/show.quiz.directive.js
@@ -1,7 +1,7 @@
 angular.module('app').directive('quizShowDirective', function() {
     return {
         restrict: 'E',
-        templateUrl: '../app/components/dashboard/quiz.overview.view.html',
+        templateUrl: '../app/components/dashboard/quizzes/quiz.overview.view.html',
         controller: 'NavbarController'
     };
 });

eaabdc7450eb8fca271dce975febdafbb204bb40
klaas <klaas.foppen@gmail.com>
2016-06-04 20:36:27 +0200

    wiens fucking hobby is het om op elk input field een ng-click te maken?

--- src/app/components/login/login.controller.js
+++ src/app/components/login/login.controller.js
@@ -1,23 +1,23 @@
 angular.module('app').controller('LoginController', function($scope, $rootScope, AuthService, $state) {
-  $scope.credentials = {
-    username: '',
-    password: ''
-  };
+    $scope.credentials = {
+        username: '',
+        password: ''
+    };
 
-  $scope.login = function(credentials) {
-    AuthService.login(credentials).then(function(res) {
-      console.log(res);
-      if (res.STATE === "SUCCEEDED") {
-        //updating the rootscope with role and  username, DONT PLACE TO MUCH IN THE ROOT!
+    $scope.login = function(credentials) {
+        AuthService.login(credentials).then(function(res) {
+            console.log(res);
+            if (res.STATE === "SUCCEEDED") {
+                //updating the rootscope with role and  username, DONT PLACE TO MUCH IN THE ROOT!
 
-        $rootScope.userRole = res.MESSAGE.Type;
-        $rootScope.userName = res.MESSAGE.Username;
-        $rootScope.name = res.MESSAGE.Name;
-        //redirect < - should be made based on mentor / child
-        $state.go("dashboard");
-      } else {
-        //throw error
-      }
-    });
-  };
+                $rootScope.userRole = res.MESSAGE.Type;
+                $rootScope.userName = res.MESSAGE.Username;
+                $rootScope.name = res.MESSAGE.Name;
+                //redirect < - should be made based on mentor / child
+                $state.go("dashboard");
+            } else {
+                //throw error
+            }
+        });
+    };
 });
